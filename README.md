69636f80c1c8d5c870d50e360574d846d06372a7
Vincent Healy <Vincent.Healy@raildeliverygroup.com>
2024-08-05 14:05:50 +0100

    Initial Commit with the 1hr complete

---
+++ README.md
@@ -0,0 +1,7 @@
+npm create vite@latest
+npm create vite@4.1.0
+
+project for folder creation
+cd to folder
+npm install
+npm run dev
---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface ButtonProps {
+    children: string;
+    color?: 'primary' |'secondary' | 'danger';
+    onClick: () => void;
+  }
+  
+  const Button = ({ children, color= 'primary', onClick }: ButtonProps) => {
+  return (
+    <button type='button' className={'btn btn-'+ color} onClick={onClick}>{children}</button>
+  )
+}
+
+export default Button
